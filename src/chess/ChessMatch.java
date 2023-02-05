a35746985ad7b9509874df4e4f08f11688e31242
Ramon Tanure Carvalho <ramon_tanure_carvalho@hotmail.com>
2023-02-05 15:34:04 -0300

    Placing pieces on the board

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -1,6 +1,9 @@
 package chess;
 
 import boardgame.Board;
+import boardgame.Position;
+import chess.pieces.King;
+import chess.pieces.Rook;
 
 public class ChessMatch {
 	
@@ -8,6 +11,7 @@ public class ChessMatch {
 	
 	public ChessMatch() {
 		this.board = new Board(8,8);
+		inicialSetup();
 	}
 	
 	public ChessPiece[][] getPieces() {
@@ -20,6 +24,13 @@ public class ChessMatch {
 		}
 		
 		return mat;
+	}
+	
+	//Responsável por iniciar a partida de chadrez colocando as peças no tabuleiro
+	private void inicialSetup() {
+		board.placePiece(new Rook(board, Color.WHITE), new Position(2, 1));
+		board.placePiece(new King(board, Color.BLACK), new Position(0, 4));
+		board.placePiece(new King(board, Color.BLACK), new Position(7, 4));
 	}
 
 }
